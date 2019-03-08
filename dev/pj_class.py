3c5ebf84f69e76f5f8dc2372549e48e60cc22ca4
Pj Zrelak <pjzrelak@u.boisestate.edu>
2019-03-07 21:10:25 -0700

    updated class exercise

--- dev/pj_class.py
+++ dev/pj_class.py
@@ -11,15 +11,30 @@ import matplotlib.pyplot as plt
 class SinePlot:
         
 
-    def __init__(self, x, f):
+    def __init__(self, x, f, xlabel='xlabel', ylabel='ylabel',sym='-',title='untitled', **kwargs):
         self.x = x
         self.freq = f
-        self.fx = np.sin(2 * np.pi * self.x * self.freq)
-        self.table = pd.DataFrame(self.fx, self.x)
-
-    def __repr__(self):
+        self.sym = sym
+        self.xtitle = xlabel
+        self.ytitle = ylabel
+        self.title = title
         
-        return repr([self.table])
+    def fx(self):
+        self.fx = np.sin(2 * np.pi * self.x * self.freq)
+        return self.fx
+        
+    def table(self): 
+        self.table = pd.DataFrame(self.fx, self.x)
+        return self.table
+        
+    
+    def mkFig(self):
+        self.fig = plt.plot(self.x, self.fx(), self.sym)
+        plt.xlabel(self.xtitle)
+        plt.ylabel(self.ytitle)
+        plt.title(self.title)
+        plt.show()
+        return self.fig
     
 
 
@@ -27,6 +42,4 @@ x = np.linspace(0, 100, num=50)
 x = x * np.pi
 
 test = SinePlot(x, 5)
-print(test)
-
-plt.plot(test.x, test.fx)
+#test.mkFig()
